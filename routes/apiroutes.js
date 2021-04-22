fe11a6747c55f42a5ab5ef1a2b7653697f15c174
amatt <amattson84@gmail.com>
2021-04-21 17:30:11 -0700

    API GET and POST Requests for notes to and from the db.

---
+++ routes/apiroutes.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+// ROUTING
+
+module.exports = function (app) {
+  // API GET Requests
+  app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) throw err;
+      dbData = JSON.parse(data);
+      res.send(dbData);
+    });
+  });
+
+  // API POST Requests
+
+  app.post("/api/notes", function (req, res) {
+    const userNotes = req.body;
+
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) throw err;
+      dbData = JSON.parse(data);
+      dbData.push(userNotes);
+      let number = 1;
+      dbData.forEach((note, index) => {
+        note.id = number;
+        number++;
+        return dbData;
+      });
+      console.log(dbData);
+
+      stringData = JSON.stringify(dbData);
+
+      fs.writeFile("./db/db.json", stringData, (err, data) => {
+        if (err) throw err;
+      });
+    });
+    res.send("Thank you for your note!");
+  });
+
