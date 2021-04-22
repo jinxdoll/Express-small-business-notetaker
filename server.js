74ca907248d470998e835e0c147d32dd8544c09d
amatt <amattson84@gmail.com>
2021-04-21 18:06:13 -0700

    Require routes for app. Listener for  app PORT.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Dependencies
-const express = require('express');
+const express = require("express");
 
 // EXPRESS Server
 const app = express();
@@ -9,4 +9,13 @@ const PORT = process.env.PORT || 8080;
 // Set up for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+// ROUTER
+
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
+
+app.listen(PORT, function () {
+  console.log(`🌎 ==> Server is now listening on port ${PORT}!`);
+});
